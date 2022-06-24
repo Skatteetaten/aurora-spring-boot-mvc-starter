1b69e389a448a2155635d9a48692db438699a264
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-06-24 12:34:04 +0200

    updated to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.6.21"
+    id("no.skatteetaten.gradle.aurora") version "4.4.24"
+}
+
+aurora {
+    useGitProperties
+    useLatestVersions
+    useVersions
+    useSonar
+    useGradleLogger
+    useJavaDefaults
+    useKotlinDefaults
+    useSpringBoot
+
+    features {
+        auroraStarters = false
+    }
+}
+
+dependencies {
+    api(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.2"))
+    api("org.springframework.cloud:spring-cloud-starter-sleuth")
+
+    api("org.springframework.boot:spring-boot-starter-web")
+    api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:1.3.12")
+    api("org.springframework.boot:spring-boot-configuration-processor")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.21")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
+    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.3.1")
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    archiveClassifier.set("boot")
+}
+
+tasks.named<Jar>("jar") {
+    archiveClassifier.set("")
+}
--- src/test/kotlin/no/skatteetaten/aurora/mvc/request/RequestMDCTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/mvc/request/RequestMDCTest.kt
@@ -50,5 +50,4 @@ class RequestMDCTest {
             assertThat(korrelasjonsid).isNotNull()
         }
     }
-
 }
