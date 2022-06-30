ace46d70bcfe6dc3e7bb453166d1550a22159a6f
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-06-30 12:34:21 +0200

    updated build gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,11 @@ dependencies {
     api("org.springframework.cloud:spring-cloud-starter-sleuth")
 
     api("org.springframework.boot:spring-boot-starter-web")
-    api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:1.3.12")
+    api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:1.3.12") {
+        exclude(group = "org.osgi", module = "org.osgi.core")
+        exclude(group = "io.zipkin.aws", module = "brave-propagation-aws")
+        exclude(group = "com.google.code.findbugs", module = "jsr305")
+    }
     api("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.21")
