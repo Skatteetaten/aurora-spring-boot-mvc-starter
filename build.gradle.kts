ded33f324c94564edd3e095112fdc82ed6b0e585
Øyvind Eriksen <oyvinder@gmail.com>
2022-11-04 15:40:51 +0100

    Feature/aos 7198 spring boot 275 (#35)
    * AOS-7198 Spring Boot 2.7.5
    
    Co-authored-by: Øyvind Eriksen <Oyvind.Eriksen2@skatteetaten.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
-    kotlin("jvm") version "1.7.10"
-    id("no.skatteetaten.gradle.aurora") version "4.5.6"
+    kotlin("jvm") version "1.7.20"
+    id("no.skatteetaten.gradle.aurora") version "4.5.10"
 }
 
 aurora {
@@ -16,15 +16,15 @@ aurora {
 
 dependencies {
     api("org.springframework.boot:spring-boot-starter-web")
-    api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:1.4.3")
+    api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:1.4.4")
     api("org.springframework.boot:spring-boot-configuration-processor")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.10")
+    testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.20")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
-    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.3.1")
+    testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.4.1")
 }
 
 tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
