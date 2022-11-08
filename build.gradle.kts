f4da8f42a50a1262e17f88054fda473bb67ae819
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-11-08 09:48:24 +0100

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,24 @@ aurora {
     features {
         auroraStarters = false
     }
+
+    versions {
+         javaSourceCompatibility = "1.8"
+    }
 }
 
 dependencies {
+    api(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.5"))
+    api("org.springframework.cloud:spring-cloud-starter-sleuth")
+    api("org.springframework.cloud:spring-cloud-sleuth-zipkin")
+
+    // strict versions to avoid conflicts
+    api("io.zipkin.brave:brave") {
+        version { strictly("5.13.9") }
+    }
+
     api("org.springframework.boot:spring-boot-starter-web")
-    api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:1.4.4")
+    api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:feature_AOS_7215-SNAPSHOT")
     api("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.20")
