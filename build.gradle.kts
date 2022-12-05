9649111cdd1762bccce9999764c5e534a1e79935
Jarle Hansen <Jarle.Hansen@skatteetaten.no>
2022-12-05 12:33:09 +0100

    fixed dependency conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,17 +21,16 @@ aurora {
 dependencies {
     api(platform("org.springframework.cloud:spring-cloud-dependencies:2021.0.5"))
     api(platform("org.springframework.cloud:spring-cloud-sleuth-otel-dependencies:1.1.0"))
+    api("org.springframework.cloud:spring-cloud-sleuth-instrumentation:3.1.5")
+    api("org.springframework.cloud:spring-cloud-sleuth-autoconfigure:3.1.5")
     api("org.springframework.cloud:spring-cloud-starter-sleuth") {
         exclude(group = "org.springframework.cloud", module = "spring-cloud-sleuth-brave")
     }
-    api("org.springframework.cloud:spring-cloud-sleuth-otel-autoconfigure")
+    api("org.springframework.cloud:spring-cloud-sleuth-otel-autoconfigure") {
+        exclude(group = "io.opentelemetry", module = "opentelemetry-api")
+        exclude(group = "io.opentelemetry", module = "opentelemetry-semconv")
+    }
     api("io.opentelemetry:opentelemetry-exporter-otlp")
-    api("io.opentelemetry:opentelemetry-api") {
-        version { strictly("1.18.0") }
-    }
-    api("io.opentelemetry:opentelemetry-context") {
-        version { strictly("1.18.0") }
-    }
 
     api("org.springframework.boot:spring-boot-starter-web")
     api("no.skatteetaten.aurora.springboot:aurora-spring-boot-base-starter:2.0.0")
@@ -40,7 +39,9 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.objenesis", module = "objenesis")
+    }
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
     testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.4.1")
     testImplementation("io.mockk:mockk:1.13.3")
